ba567163fab4c6cc3e281b724114c53f4cf4ea8b
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2022-10-17 15:47:01 -0700

    Add functions for mult, div, sub.  Add window.alert that displays results for all 4 functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,33 @@
-function saySomething(whatToSay) {
-  window.alert(whatToSay);
-}
-
+//business logic
 function add(number1, number2) {
   return number1 + number2;
 }
 
-saySomething("hi");
+function subtraction(number1, number2) {
+  return number1 - number2;
+}
 
-const result = add(3, 5);
-window.alert(result);
+function multiplication(number1, number2) {
+  return number1 * number2;
+}
+
+function division(number1, number2) {
+  return number1 / number2;
+}
+
+//user interface logic
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter a second number:"));
+
+const addResult = number1 + " + " + number2 + " = " + add(number1,number2) + "\r";
+const subResult = number1 + " - " + number2 + " = " + subtraction(number1, number2) + "\r";
+const multResult = number1 + " * " + number2 + " = " + multiplication(number1, number2) + "\r";
+const divResult = number1 + " / " + number2 + " = " + division(number1, number2) + "\r";
+
+//window.alert(add(number1, number2));
+window.alert(addResult + subResult + multResult + divResult);
+//window.alert(addResult);
+
 
 
 
